ef91ed7d2da1922287ea65e979a6015fbc9a7b51
Tim Korse <t.korse@korsit.com>
2018-09-30 13:08:27 +0200

    Added some comments

--- src/index.js
+++ src/index.js
@@ -5,19 +5,21 @@ const defaultOptions = require('./constants/options');
 
 module.exports = function (moduleOptions) {
 
+    // Accept module options as function:
     if (typeof moduleOptions == 'function') {
         moduleOptions = moduleOptions(session);
     }
-
-    const app = express();
-
+    
+    // Merge options with default options:
     const options = {
         
         ...defaultOptions,
         ...moduleOptions
     };
 
-    // 
+    // Setup the session middleware:
+    const app = express();
+
     app.use(session(options));
 
     // Add server middleware to the beginning, so that is accessible in every middleware handler:
